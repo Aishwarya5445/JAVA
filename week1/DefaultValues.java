139f5247f332d3a8dffdf2ad0f3a006972b6d8e1
Aishwarya_5445 <kaishwarya5445@gmail.com>
2026-07-19 13:34:37 +0530

    updated programs

--- week1/DefaultValues.java
+++ week1/DefaultValues.java
@@ -10,12 +10,12 @@ public class DefaultValues {
     public static void main(String args[]){
         System.out.println("Default value of all primitive data types of java:");
         System.out.println("byte:"+b);
-        System.out.println(" short:"+s);
+        System.out.println("short:"+s);
         System.out.println("int:"+i);
-        System.out.println(" long:"+l);
-        System.out.println(" float:"+f);
-        System.out.println(" double:"+d);
-        System.out.println(" char:["+c+"]");
-        System.out.println(" boolean:"+bool);
+        System.out.println("long:"+l);
+        System.out.println("float:"+f);
+        System.out.println("double:"+d);
+        System.out.println("char:["+c+"]");
+        System.out.println("boolean:"+bool);
     }
 }
--- week2/MethodOverload.java
+++ week2/MethodOverload.java
@@ -12,8 +12,8 @@ public class MethodOverload{
     public static void main(String args[])
     {
         System.out.println("::Method Overloading::");
-        System.out.println("adding two numbers using add method"+Adder.add(11,11));
-        System.out.println("adding three numbers using add method"+Adder.add(11,11,11));
+        System.out.println("adding two numbers using add method:"+Adder.add(11,11));
+        System.out.println("adding three numbers using add method:"+Adder.add(11,11,11));
 
     }
 }
